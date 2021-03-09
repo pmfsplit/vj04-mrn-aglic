8c41c1f6358bbaae3fa8dca2653011f64b072344
mrn-aglic <mrn.aglic@gmail.com>
2021-03-09 19:47:00 +0100

    Hello actor with lambda

--- BecomePrimjer/WindowsFormsApp2/HelloActor.cs
+++ BecomePrimjer/WindowsFormsApp2/HelloActor.cs
@@ -6,32 +6,38 @@ namespace WindowsFormsApp2
     public class HelloActor : ReceiveActor
     {
         int br = 0;
-        Label _label;
+        //Label _label;
         public HelloActor(Label label)
         {
-            _label = label;
-            NeLogiranoPonasanje();
+            //_label = label;
+            NeLogiranoPonasanje(label);
         }
 
-        private void NeLogiranoPonasanje()
+        private void NeLogiranoPonasanje(Label _label)
         {
             Receive<Start>(x => _label.Text = "Ovdje ce se ispisat rezultat...");
             Receive<Message>(x =>
             {
                 br++;
                 _label.Text = $"{x.Sadrzaj} {br}";
-                Become(LogiranoPonasanje);
+                Become(() => LogiranoPonasanje(_label));
             });
         }
 
-        private void LogiranoPonasanje()
+        private void LogiranoPonasanje(Label _label)
         {
             Receive<Message>(x => _label.Text = "Vec ste logirani...");
             Receive<Logout>(x =>
             {
                 _label.Text = "Odlogirani ste";
-                Become(NeLogiranoPonasanje);
+                Become(() => NeLogiranoPonasanje(_label));
             });
+        }
+
+        protected override void Unhandled(object message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+            base.Unhandled(message);
         }
     }
 }
