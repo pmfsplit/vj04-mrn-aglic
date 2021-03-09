140405e5107bb5546eb3df52ff91c64f6f227616
mrn-aglic <mrn.aglic@gmail.com>
2021-03-09 19:47:48 +0100

    Hello actor without lamda

--- BecomePrimjer/WindowsFormsApp2/HelloActor.cs
+++ BecomePrimjer/WindowsFormsApp2/HelloActor.cs
@@ -6,31 +6,31 @@ namespace WindowsFormsApp2
     public class HelloActor : ReceiveActor
     {
         int br = 0;
-        //Label _label;
+        Label _label;
         public HelloActor(Label label)
         {
-            //_label = label;
-            NeLogiranoPonasanje(label);
+            _label = label;
+            NeLogiranoPonasanje();
         }
 
-        private void NeLogiranoPonasanje(Label _label)
+        private void NeLogiranoPonasanje()
         {
             Receive<Start>(x => _label.Text = "Ovdje ce se ispisat rezultat...");
             Receive<Message>(x =>
             {
                 br++;
                 _label.Text = $"{x.Sadrzaj} {br}";
-                Become(() => LogiranoPonasanje(_label));
+                Become(LogiranoPonasanje);
             });
         }
 
-        private void LogiranoPonasanje(Label _label)
+        private void LogiranoPonasanje()
         {
             Receive<Message>(x => _label.Text = "Vec ste logirani...");
             Receive<Logout>(x =>
             {
                 _label.Text = "Odlogirani ste";
-                Become(() => NeLogiranoPonasanje(_label));
+                Become(NeLogiranoPonasanje);
             });
         }
 
